8b38f99505d44238c679ab00ab4287632fa10385
PrafulSingha <34261487+PrafulSingha@users.noreply.github.com>
2017-12-06 12:50:23 +0530

    updated

---
+++ src/com/ace/StudentException/ControllerException.java
@@ -0,0 +1,38 @@
+package com.ace.StudentException;
+
+public class ControllerException extends StudentException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8837752977091273677L;
+
+	public ControllerException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ControllerException(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ControllerException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ControllerException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ControllerException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
---
+++ src/com/ace/StudentException/StudentException.java
@@ -0,0 +1,40 @@
+package com.ace.StudentException;
+
+public class StudentException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8686376136189740405L;
+
+	public StudentException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public StudentException(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public StudentException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public StudentException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public StudentException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+	
+
+}
