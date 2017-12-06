ffe0166fe7a9cf1b2f2d71bca2c80251b36725e2
PrafulSingha <34261487+PrafulSingha@users.noreply.github.com>
2017-12-06 11:33:21 +0530

    updated

--- src/com/ace/entity/Student.java
+++ src/com/ace/entity/Student.java
@@ -3,6 +3,8 @@
  */
 package com.ace.entity;
 
+import java.util.List;
+
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
@@ -16,7 +18,10 @@ public class Student {
 	
 	private int studentId;
 	private String studentName;
-	private String studentClass;
+	private int studentClass;
+	
+	
+	private List<Subject> subjectList;
 	
 	public int getStudentId() {
 		return studentId;
@@ -32,51 +37,22 @@ public class Student {
 	public void setStudentName(String studentName) {
 		this.studentName = studentName;
 	}
-	public String getStudentClass() {
+	public int getStudentClass() {
 		return studentClass;
 	}
 	@XmlElement
-	public void setStudentClass(String studentClass) {
+	public void setStudentClass(int studentClass) {
 		this.studentClass = studentClass;
 	}
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((studentClass == null) ? 0 : studentClass.hashCode());
-		result = prime * result + studentId;
-		result = prime * result + ((studentName == null) ? 0 : studentName.hashCode());
-		return result;
+	public List<Subject> getSubjectList() {
+		return subjectList;
 	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Student other = (Student) obj;
-		if (studentClass == null) {
-			if (other.studentClass != null)
-				return false;
-		} else if (!studentClass.equals(other.studentClass))
-			return false;
-		if (studentId != other.studentId)
-			return false;
-		if (studentName == null) {
-			if (other.studentName != null)
-				return false;
-		} else if (!studentName.equals(other.studentName))
-			return false;
-		return true;
-	}
-	@Override
-	public String toString() {
-		return "Student [studentId=" + studentId + ", studentName=" + studentName + ", studentClass=" + studentClass
-				+ "]";
+	@XmlElement(name="Subject" , type=Subject.class)
+	public void setSubjectList(List<Subject> subjectList) {
+		this.subjectList = subjectList;
 	}
 	
 	
-
+	
+	
 }
