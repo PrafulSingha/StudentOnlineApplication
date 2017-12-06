7062cbfa692a35b967f29739a7e1a4e2e6fa1fdd
PrafulSingha <34261487+PrafulSingha@users.noreply.github.com>
2017-12-06 11:33:50 +0530

    updated

--- src/com/ace/entity/Subject.java
+++ src/com/ace/entity/Subject.java
@@ -14,16 +14,15 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement
 public class Subject {
 	
-	private String subjectId;
+	private int subjectId;
 	private String subjectName;
 	private int subjectMarks;
 	
-		
-	public String getSubjectId() {
+	public int getSubjectId() {
 		return subjectId;
 	}
 	@XmlAttribute
-	public void setSubjectId(String subjectId) {
+	public void setSubjectId(int subjectId) {
 		this.subjectId = subjectId;
 	}
 	public String getSubjectName() {
@@ -39,43 +38,6 @@ public class Subject {
 	@XmlElement
 	public void setSubjectMarks(int subjectMarks) {
 		this.subjectMarks = subjectMarks;
-	}
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((subjectId == null) ? 0 : subjectId.hashCode());
-		result = prime * result + subjectMarks;
-		result = prime * result + ((subjectName == null) ? 0 : subjectName.hashCode());
-		return result;
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Subject other = (Subject) obj;
-		if (subjectId == null) {
-			if (other.subjectId != null)
-				return false;
-		} else if (!subjectId.equals(other.subjectId))
-			return false;
-		if (subjectMarks != other.subjectMarks)
-			return false;
-		if (subjectName == null) {
-			if (other.subjectName != null)
-				return false;
-		} else if (!subjectName.equals(other.subjectName))
-			return false;
-		return true;
-	}
-	@Override
-	public String toString() {
-		return "Subject [subjectId=" + subjectId + ", subjectName=" + subjectName + ", subjectMarks=" + subjectMarks
-				+ "]";
 	}
 	
 	
